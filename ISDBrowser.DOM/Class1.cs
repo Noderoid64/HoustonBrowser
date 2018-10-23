c3d72ffa6bbc3ae5c8f36dc2a1e835c286116094
NYehor <https:/github.com/horba/git-workshop>
2018-10-23 22:23:26 +0300

    [Update] Create project

---
+++ ISDBrowser.DOM/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ISDBrowser.DOM
+{
+    public class Class1
+    {
+    }
+}
---
+++ ISDBrowser.DOM/INode.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace ISDBrowser.DOM
+{
+    interface INode
+    {
+    }
+}
