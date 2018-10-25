ef3b094bd2e0db0691557f375abd6a847de7bf93
eugenezhornyak <eugenezhornyak@gail.com>
2018-10-25 17:46:09 +0300

    Project renamed

--- ISDBrowser.Network.Test/obj/Debug/netcoreapp2.1/ISDBrowser.Network.Test.AssemblyInfo.cs
+++ ISDBrowser.Network.Test/obj/Debug/netcoreapp2.1/ISDBrowser.Network.Test.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ISDBrowser.HTMLParser.Tests")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ISDBrowser.Network.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("ISDBrowser.HTMLParser.Tests")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ISDBrowser.HTMLParser.Tests")]
+[assembly: System.Reflection.AssemblyProductAttribute("ISDBrowser.Network.Test")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ISDBrowser.Network.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
