6f07dc030fd105d770413fdd15094907f21c13e6
Misha Krygin <Noderoid64@gmail.com>
2018-10-28 19:54:57 +0300

    [Update] add general test

--- HoustonBrowser.Network.Test/TestClass.cs
+++ HoustonBrowser.Network.Test/TestClass.cs
@@ -9,14 +9,21 @@ namespace HoustonBrowser.HttpModule.Test
 {
     public class TestClass
     {
+<<<<<<< HEAD
         //http://www.netside.net/boba/webmasters.html
         //192.168.0.110
         //houstonbrowsertest.ddns.net
+=======
+>>>>>>> [Update] add general test
         [Fact]
         public void TestVoid(){
             IHttpClient client = new HttpClient();
 
+<<<<<<< HEAD
             string real = client.GetHtml("http://www.netside.net/boba/webmasters.html");
+=======
+            string real = client.GET("http://www.netside.net/boba/webmasters.html");
+>>>>>>> [Update] add general test
 
             Assert.NotEmpty(real);
         }
