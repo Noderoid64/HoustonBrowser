9477fe025db986c8cd63366892d4a8202f67ef1b
Мария Дегтяренко <mdegtyarenko725@gmail.com>
2018-11-06 17:15:16 +0200

    controls changed

--- HoustonBrowser.Controls/Controls/Button.cs
+++ HoustonBrowser.Controls/Controls/Button.cs
@@ -9,6 +9,24 @@ namespace HoustonBrowser.Controls
 {
     public class Button: BrowserControl
     {
-       
+       public override void Render(DrawingContext context)
+        {
+            if(this.IsDefault)
+            {
+                SetDefaultStyles();
+            }
+            base.Render(context);
+        }       
+
+        private void SetDefaultStyles()
+        {
+            this.BackgroundBrush = new SolidColorBrush(new Color(145,220,66,0));
+            this.Width=this.Height=30;
+            this.Form=new RectangleGeometry(new Rect(this.Left,this.Top,this.Width,this.Height));
+            this.TextTypeface=new Typeface("Arial", 10);
+            this.ForegroundBrush=new SolidColorBrush(new Color(255,0,0,0));
+            this.AlignText=TextAlignment.Center;
+
+        }
     }
 }
--- HoustonBrowser.Controls/Controls/TextBox.cs
+++ HoustonBrowser.Controls/Controls/TextBox.cs
@@ -29,5 +29,25 @@ namespace HoustonBrowser.Controls
         {
                 KeyUp?.Invoke(sender, e);
         } 
+
+        public override void Render(DrawingContext context)
+        {
+            if(this.IsDefault)
+            {
+                SetDefaultStyles();
+            }
+            base.Render(context);
+        }       
+
+        private void SetDefaultStyles()
+        {
+            this.BackgroundBrush = new SolidColorBrush(new Color(145, 203,218,41));
+            this.Width=this.Height=30;
+            this.Form=new RectangleGeometry(new Rect(this.Left,this.Top,this.Width,this.Height));
+            this.TextTypeface=new Typeface("Arial", 10);
+            this.ForegroundBrush=new SolidColorBrush(new Color(255,0,0,0));
+            this.AlignText=TextAlignment.Center;
+
+        }
     }
 }
