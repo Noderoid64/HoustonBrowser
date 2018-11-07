5c19e5edfbb3cf1a04b544f1f0e8baa8c941d58e
Ivan Paniotov <ivanpaniotov@gmail.com>
2018-11-08 01:30:26 +0200

    Passing data from script tag to js module

--- HoustonBrowser.Core/Core.cs
+++ HoustonBrowser.Core/Core.cs
@@ -30,7 +30,7 @@ namespace HoustonBrowser.Core
         {
             this.ui = ui;
             this.httpClient = new MockHttpClient();
-            this.parser = new mockParser();
+            this.parser = new Parser();
             this.control=new BrowserControl();
             this.js=new MockJS();
             this.dom = new MockDocument();
@@ -38,17 +38,19 @@ namespace HoustonBrowser.Core
             ui.onKeyDown += Ui_onKeyDown;
             ui.onMouseClick += Ui_onMouseClick;
             ui.onPageLoad += Ui_onPageLoad;
+            parser.onNonHtmlEvent += Parser_onNonHtmlEvent;
 
-            // button.Click+=Button_onMouseClick;
+            
+        }
 
-            parser = new Parser();
-            var doc = parser.Parse("<html>\r\n<head>\r\nHoustonBrowser\r\n</head>\r\n<body>\r\n<script>\r\nfunction myFunction() {\r\n    var x = document.getElementById(\"myDIV\");\r\n    if (x) {\r\n      x.style.display = \"none\";\r\n    }\r\n}\r\n</script>\r\n\r\n<button onclick=\"myFunction()\">Click Me</button>\r\n\r\n<div id=\"myDIV\">\r\n  This is my DIV element.\r\n</div>\r\n</body>\r\n</html>");
-            //var renderTree = new RenderTree(doc);
-            //renderTree.GetPage();
+        private void Parser_onNonHtmlEvent(object sender, string e)
+        {
+            js.Process(e);
         }
 
         private void Ui_onPageLoad(object sender, object e)
         {
+            var doc = parser.Parse("<html>\r\n<head>\r\nHoustonBrowser\r\n</head>\r\n<body>\r\n<script>\r\nalert(TEST)\r\n</script>\r\n\r\n<button onclick=\"myFunction()\">Click Me</button>\r\n\r\n<div id=\"myDIV\">\r\n  This is my DIV element.\r\n</div>\r\n</body>\r\n</html>");
 
         }
 
