9f48adc17323bc58e0defdb48f883c97970bbc1e
eugenezhornyak <38474637+eugenezhornyak@users.noreply.github.com>
2018-11-08 02:01:12 +0200

    Chain Update (#63)

--- HoustonBrowser.Core/Core.cs
+++ HoustonBrowser.Core/Core.cs
@@ -29,8 +29,10 @@ namespace HoustonBrowser.Core
         public Core(IUI ui)
         {
             this.ui = ui;
-            this.httpClient = new MockHttpClient();
+
+            this.httpClient = new HttpClient();
             this.parser = new Parser();
+
             this.control=new BrowserControl();
             this.js=new MockJS();
             this.dom = new MockDocument();
@@ -42,7 +44,7 @@ namespace HoustonBrowser.Core
 
             // button.Click+=Button_onMouseClick;
 
-            parser = new Parser();
+
             var doc = parser.Parse("<html>\r\n<head>\r\nHoustonBrowser\r\n</head>\r\n<body>\r\n<script>\r\nfunction myFunction() {\r\n    var x = document.getElementById(\"myDIV\");\r\n    if (x) {\r\n      x.style.display = \"none\";\r\n    }\r\n}\r\n</script>\r\n\r\n<button onclick=\"myFunction()\">Click Me</button>\r\n\r\n<div id=\"myDIV\">\r\n  This is my DIV element.\r\n</div>\r\n</body>\r\n</html>");
             var renderTree = new RenderTree(doc);
             var tmp = renderTree.GetPage();
@@ -54,6 +56,10 @@ namespace HoustonBrowser.Core
             js.Process(e);
         }
 
+        public void PageLoading(string url)
+        {
+            parser.Parse(httpClient.GetHtml(url));
+        }
         private void Ui_onPageLoad(object sender, object e)
         {
             var doc = parser.Parse("<html>\r\n<head>\r\nHoustonBrowser\r\n</head>\r\n<body>\r\n<script>\r\nalert(TEST)\r\n</script>\r\n\r\n<button onclick=\"myFunction()\">Click Me</button>\r\n\r\n<div id=\"myDIV\">\r\n  This is my DIV element.\r\n</div>\r\n</body>\r\n</html>");
