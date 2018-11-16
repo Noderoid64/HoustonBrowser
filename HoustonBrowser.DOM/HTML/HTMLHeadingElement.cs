c8e5858c5e568cd92b392c89e874edaf38603fc5
NYehor <y.nedbalo@outlook.com>
2018-11-16 23:14:56 +0200

    added class

---
+++ HoustonBrowser.DOM/HTML/HTMLHeadingElement.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using HoustonBrowser.DOM;
+using HoustonBrowser.DOM.Interface;
+
+namespace HoustonBrowser.DOM
+{
+    public class HTMLHeadingElement : Element
+    {
+        public string Alighn { get; set; }
+        public HTMLHeadingElement() : base("888") { }
+    }
+}
