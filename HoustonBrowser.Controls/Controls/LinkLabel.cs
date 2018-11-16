e97b6f4a2f789388623a296da3d16bf6ad580223
Мария Дегтяренко <mdegtyarenko725@gmail.com>
2018-11-16 18:04:47 +0200

    label added

--- HoustonBrowser.Controls/Controls/LinkLabel.cs
+++ HoustonBrowser.Controls/Controls/LinkLabel.cs
@@ -17,22 +17,15 @@ namespace HoustonBrowser.Controls
             {
                 this.Form=new RectangleGeometry(new Rect(this.Left,this.Top,this.Width,this.Height));
             }
-
-            if(this.IsPressed)
-            {
-                ForegroundBrush = new SolidColorBrush(new Color(255, 51, 0, 102));
-            }
-
             Pen underlinePen=new Pen(this.ForegroundBrush);
 
             var lines = FormattedText.GetLines().ToList();
             int start = 0;
             for(int i=0; i<lines.Count; i++)
             {
-                var bounds1 = this.FormattedText.HitTestTextPosition(start);
-                var bounds2 = this.FormattedText.HitTestTextPosition(start+lines[i].Length-1);
-                context.DrawLine(underlinePen, bounds1.BottomLeft+new Point(Left,Top+Height/2), bounds2.BottomRight+new Point(Left, Top+Height/2));
-                start+=lines[i].Length;
+                var bounds = this.FormattedText.HitTestTextRange(start, lines[i].Length).ToList();
+                context.DrawLine(underlinePen, bounds[0].BottomLeft+new Point(Left,Top), bounds[0].BottomRight+new Point(Left, Top));
+                start=lines[i].Length;
             }
 
             base.Render(context);
