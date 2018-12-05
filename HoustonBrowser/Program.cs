97fce32ba4ab8809aac51fd56035a558786e54cd
Misha Krygin <Noderoid64@gmail.com>
2018-12-05 11:01:08 +0200

    Add logger

--- HoustonBrowser/Program.cs
+++ HoustonBrowser/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using MercuryLogger;
+using MercuryLogger.Extentions;
+using MercuryLogger.Loggers;
 using Avalonia;
 using Avalonia.Logging.Serilog;
 
@@ -8,7 +11,24 @@ namespace HoustonBrowser
     {
         static void Main(string[] args)
         {
+            ConfigLogger();
             BuildAvaloniaApp().Start<MainWindow>();
+        }
+
+        static void ConfigLogger()
+        { // remove in release
+            MainLogger logger = MainLogger.GetInstance();
+            TimeExtention timeExtention = new TimeExtention()
+            {
+                ShowHour = true,
+                ShowMinute = true,
+                ShowSecond = true,
+                ShowMillisecond = true
+            };
+            FileLogger fileLogger = new FileLogger("./", "GlobalLog.txt");
+            logger.AddLogger(timeExtention);
+            timeExtention.AddLogger(fileLogger);
+            logger.Log("<--------Logger-Start-------->");
         }
 
         public static AppBuilder BuildAvaloniaApp()
