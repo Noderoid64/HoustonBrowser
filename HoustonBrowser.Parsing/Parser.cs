a7aa335769d5f023e3656c09e2bf2927a006f0d1
Ivan Paniotov <ivanpaniotov@gmail.com>
2018-12-05 21:03:14 +0200

    Update Parser.cs

--- HoustonBrowser.Parsing/Parser.cs
+++ HoustonBrowser.Parsing/Parser.cs
@@ -12,10 +12,6 @@ namespace HoustonBrowser.Parsing
         HTMLDocument doc;
         public Parser()
         {
-            StatesData.InBody.onNonHtmlEvent += (caller, args) =>
-            {
-                onNonHtmlEvent(caller, args);
-            };
         }
 
         public event EventHandler<string> onNonHtmlEvent;
@@ -39,7 +35,10 @@ namespace HoustonBrowser.Parsing
             State state = new State();//main parse class
             StatesData.Reload();//IF SOME PROBLEMS DETECTED CHECK THIS METHOD
             StatesData.SetData(nodes, doc);//Adding data
-
+            StatesData.InBody.onNonHtmlEvent += (caller, args) =>
+            {
+                onNonHtmlEvent(caller, args);
+            };
             while (!StatesData.IsLast)
             {
                 lexAnalyser.InsertionState=StatesData.currentState;
